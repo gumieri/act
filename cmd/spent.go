f0cd33375b662563d18c4ad5988e9fff18c3556a
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2017-09-12 13:47:51 -0300

    Add spent command

---
+++ cmd/spent.go
@@ -0,0 +1,116 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type TimeEntryStruct struct {
+	IssueId    int     `json:"issue_id"`
+	Date       string  `json:"spent_on"`
+	Time       float64 `json:"hours"`
+	ActivityId int     `json:"activity_id"`
+	Comment    string  `json:"comments"`
+}
+
+type PayloadStruct struct {
+	TimeEntry TimeEntryStruct `json:"time_entry"`
+}
+
+var timeEntry TimeEntryStruct
+
+func spentRun(cmd *cobra.Command, args []string) {
+	var err error
+
+	timeEntry.Time, err = strconv.ParseFloat(args[0], 64)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	payload := new(PayloadStruct)
+	payload.TimeEntry = timeEntry
+
+	marshal, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := fmt.Sprintf("http://%s/time_entries.json", viper.Get("redmine.url"))
+	payloadMarshal := bytes.NewBuffer(marshal)
+	request, err := http.NewRequest(http.MethodPost, url, payloadMarshal)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.Header.Add("X-Redmine-API-Key", fmt.Sprintf("%s", viper.Get("redmine.access_key")))
+	request.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusCreated {
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Fatal(response.Status, "\n", string(bodyBytes))
+	}
+
+	log.Printf("Added %.2f hour(s) to the Issue #%d.", timeEntry.Time, timeEntry.IssueId)
+	fmt.Println()
+}
+
+// spentCmd represents the spent command
+var spentCmd = &cobra.Command{
+	Use:   "spent",
+	Short: "Update an Issue defining the time spent on it.",
+	Long:  ``,
+	Args:  cobra.MinimumNArgs(1),
+	Run:   spentRun,
+}
+
+func init() {
+	RootCmd.AddCommand(spentCmd)
+
+	current_date := time.Now().Local().Format("2006-01-02")
+
+	spentCmd.Flags().IntVarP(&timeEntry.IssueId, "issue", "i", 0, "The Issue ID.")
+	spentCmd.Flags().StringVarP(&timeEntry.Date, "date", "d", current_date, "The date when the time was spent on.")
+	spentCmd.Flags().IntVar(&timeEntry.ActivityId, "activity_id", 0, "The Activity ID.")
+	spentCmd.Flags().StringVarP(&timeEntry.Comment, "comment", "m", "", "A short description of what was done.")
+}
