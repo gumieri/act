a94db1aa4f4e0a668a7264f6b480b113d3cf814e
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2017-09-20 17:44:24 -0300

    Fix a bug when removeSharp was false

--- lib/editor/main.go
+++ lib/editor/main.go
@@ -9,6 +9,13 @@ import (
 	"strings"
 )
 
+// Open the specified editor
+/* Parameters:
+ * editorCommand
+ * fileName: the name of the temporary file
+ * helperText: a text to be inserted on the file to be edited
+ * removeSharps: a boolean to remove or not any line starting with #
+ */
 func Open(editorCommand string, fileName, helperText string, removeSharps bool) (text string, err error) {
 	filePath := fmt.Sprintf("%s/%s", os.TempDir(), fileName)
 
@@ -44,10 +51,14 @@ func Open(editorCommand string, fileName, helperText string, removeSharps bool) 
 		return
 	}
 
+	text = string(content)
+
 	if removeSharps {
 		re := regexp.MustCompile("(?m)[\r\n]+^#.*$")
-		text = strings.Trim(re.ReplaceAllString(string(content), ""), "\n")
+		text = re.ReplaceAllString(text, "")
 	}
+
+	text = strings.Trim(text, "\n")
 
 	return
 }
